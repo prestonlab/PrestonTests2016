102bd9da04e2589dbee917b04801f91edd874a13
brockuniera <brockuniera@utexas.edu>
2016-11-03 10:15:49 -0500

    Add LandmarkSpawner.cs diff --git a/Assets/Scripts/LandmarkSpawner.cs b/Assets/Scripts/LandmarkSpawner.cs new file mode 100644 index 0000000..b43a9ce --- /dev/null +++ b/Assets/Scripts/LandmarkSpawner.cs @@ -0,0 +1,17 @@ +﻿using UnityEngine; +using System.Collections; + +public class LandmarkSpawner : MonoBehaviour { +    public void Start(){ +        HideLandmark(); +    } + +    public void ShowLandmark(int index){ +        transform.GetChild(index).gameObject.SetActive(true); +    } + +    public void HideLandmark(){ +        foreach(Transform child in transform) +            child.gameObject.SetActive(false); +    } +} diff --git a/Assets/Scripts/LandmarkSpawner.cs.meta b/Assets/Scripts/LandmarkSpawner.cs.meta new file mode 100644 index 0000000..e377eb5 --- /dev/null +++ b/Assets/Scripts/LandmarkSpawner.cs.meta @@ -0,0 +1,12 @@ +fileFormatVersion: 2 +guid: 0919f5b062adea540a87f90972fd7829 +timeCreated: 1478185769 +licenseType: Free +MonoImporter: +  serializedVersion: 2 +  defaultReferences: [] +  executionOrder: 0 +  icon: {instanceID: 0} +  userData: +  assetBundleName: +  assetBundleVariant:

---
+++ Assets/Scripts/LandmarkSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandmarkSpawner : MonoBehaviour {
+    public void Start(){
+        HideLandmark();
+    }
+
+    public void ShowLandmark(int index){
+        transform.GetChild(index).gameObject.SetActive(true);
+    }
+
+    public void HideLandmark(){
+        foreach(Transform child in transform)
+            child.gameObject.SetActive(false);
+    }
+}
