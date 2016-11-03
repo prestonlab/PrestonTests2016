55f1004ca31df09cb6c89043c038bf017f228f68
brockuniera <brockuniera@utexas.edu>
2016-11-03 04:58:01 -0500

    Colors for mountains, square brush, some empty logic gos

---
+++ Assets/Scripts/SpawnLogic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnLogic : MonoBehaviour {
+	void Awake() {
+        foreach(Transform child in transform){
+            child.LookAt(transform);
+        }
+	}
+}
